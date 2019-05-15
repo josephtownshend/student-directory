eba49536605d3a8d5f495c6059e67808e8b459e2
joseph townshend <joetownshend@mac.com>
2019-05-15 12:20:21 +0100

    Addition of an Array

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
 student_count = 11
 
 # First we print a list of all the students.
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
+
 puts "The students of the Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
+
 # Finally we print the total number of students.
 puts "Overall, we have #{student_count} great students"
-# It's important that print() doesnt add new line chars.
